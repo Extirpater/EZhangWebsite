57ce57eba5f325c09ed018904c141fabb646c867
Extirpater <edwardbzhang@gmail.com>
2023-07-03 22:03:31 -0400

    last time cmon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 const Hero = () => {
+  
   return (
     <section className={` relative w-full h-screen mx-auto`}>
       <div
