92159d41fb2abcb593f9fb313106117726ffce2f
Extirpater <edwardbzhang@gmail.com>
2023-07-03 22:41:37 -0400

    testing again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 const Hero = () => {
-  
   return (
     <section className={` relative w-full h-screen mx-auto`}>
       <div
